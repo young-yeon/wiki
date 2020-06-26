8380a377f2748a2941afda6acce05a98be661b3f
young-yeon <as608683@naver.com>
2020-06-26 18:34:52 +0900

    2020-06-26

--- controll/auth/index.js
+++ controll/auth/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("./auth.ctrl");
+const mailCtrl = require("./mail.ctrl");
+const { mail } = require("./mail.ctrl");
 
 router.get("/login", ctrl.loginPage);
 router.post("/login", ctrl.login);
 router.get("/logout", ctrl.logout);
 router.get("/register", ctrl.registerPage);
 router.post("/register", ctrl.register);
+
+router.post("/emailChk", mailCtrl.mail);
+router.get("/emailChk", mailCtrl.mailChk);
 
 module.exports = router;
