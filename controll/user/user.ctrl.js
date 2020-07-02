52422913e3861ac7deaf714bce4221bd2fc450ac
young-yeon <as608683@naver.com>
2020-07-02 15:01:58 +0900

    2020-07-02

--- controll/user/user.ctrl.js
+++ controll/user/user.ctrl.js
@@ -1,12 +1,20 @@
-const UserModel = require("../../models/user");
+const ContModel = require("../../models/contribution");
 
 const userPage = (req, res) => {
   const nickname = req.session.nickname;
   const accLevel = req.session.accessLevel || -1;
-  res.render("user/index", {
-    nickname,
-    accLevel,
-  });
+  const creator_id = req.session._id;
+
+  ContModel.find({ creator_id }, (err, cont) => {
+    if (err) return res.status(500).end();
+    res.render("user/index", {
+      nickname,
+      accLevel,
+      cont,
+    });
+  })
+    .sort({ _id: -1 })
+    .limit(7);
 };
 
 module.exports = { userPage };
